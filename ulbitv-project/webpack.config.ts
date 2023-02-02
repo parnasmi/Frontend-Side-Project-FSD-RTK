534f7abfacbadf0a15e9757b4d36f4cbb3e432d1
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-02-02 07:50:00 +0500

    1-webpack/add react support to webpack config

--- ulbitv-project/webpack.config.ts
+++ ulbitv-project/webpack.config.ts
@@ -6,7 +6,7 @@ import { BuildPaths, buildWebpackConfigs,BuildEnv } from './config/build';
 export default (env:BuildEnv) => {
 
     const paths:BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         html: path.resolve(__dirname, 'public', 'index.html'),
         build:path.resolve(__dirname, 'build'),
     }
