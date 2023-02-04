2d829900a9b4961241f0a0a9b452b97c6eb85266
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-02-04 09:54:04 +0500

    1_module:webpack: 6_lesson: code splitting

--- ulbitv-project/src/App.tsx
+++ ulbitv-project/src/App.tsx
@@ -1,10 +1,32 @@
-import { Counter } from "./components/Counter/Counter";
+import { Link, Route, Routes } from "react-router-dom";
+import {lazy, Suspense} from 'react';
+
+
+function lazyLoadingTimeout<T>(dynamicImport: Promise<T>, countMSec: number): Promise<T>{
+    return new Promise((resolve)=>{
+        setTimeout(()=>{resolve(dynamicImport)}, countMSec);
+    })
+};
+
+const AboutPage = lazy(() => lazyLoadingTimeout(import('./pages/AboutPage'), 1000));
+const MainPage = lazy(() => lazyLoadingTimeout(import('./pages/MainPage'), 1000));
+
 import './style.scss'; 
 export default function App() {
 
     return (
         <>
-        <Counter/>
+            <ul>
+                <li><Link to="/">Homepage</Link></li>
+                <li><Link to="/about">AboutPage</Link></li>
+            </ul>
+            <Suspense fallback={<>Loading</>}>
+            <Routes>
+                
+                <Route path="/" element={<MainPage/>}/>
+                <Route path="/about" element={<AboutPage/>}/>
+            </Routes>
+            </Suspense>
         </>
     )
 }
