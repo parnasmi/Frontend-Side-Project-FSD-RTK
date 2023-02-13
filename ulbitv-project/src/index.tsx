b552d56d6171f97effc3a9e054408d493a436477
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-02-13 07:30:26 +0500

    3-module: add ErrorBoundary

--- ulbitv-project/src/index.tsx
+++ ulbitv-project/src/index.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import App from 'app/App';
 import { ThemeProvider } from 'shared/contexts/theme-context';
 import 'shared/config/i18n/i18n';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 const providers = (
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>
 );
 
