984ff3652b876e2d38f34fea8445d67dcfcf1163
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-02-22 22:07:16 +0500

    change refs

--- next_react_project/src/shared/ui/Modal/Modal.stories.tsx
+++ next_react_project/src/shared/ui/Modal/Modal.stories.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
@@ -17,12 +16,12 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
     isOpen: true,
-    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam. possimus quis saepe sunt totam\n ',
+    children: 'Hello',
 };
 
 export const Dark = Template.bind({});
 Dark.args = {
     isOpen: true,
-    children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam. possimus quis saepe sunt totam\n ',
+    children: 'Hello',
 };
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
