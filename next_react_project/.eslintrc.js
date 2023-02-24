1f1a97c31a09596cda597c7b511752bad86c3b72
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-02-24 19:09:31 +0500

    4-module: turn on dark modal theme storybook 4

--- next_react_project/.eslintrc.js
+++ next_react_project/.eslintrc.js
@@ -47,7 +47,6 @@ module.exports = {
         'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
         'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
         'no-param-reassign': 'off',
-        'linebreak-style': 0,
     },
     globals: {
         __IS_DEV__: true,
