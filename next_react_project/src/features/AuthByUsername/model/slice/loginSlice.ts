162a6bc5300e10b4b68a9dd3ac20ea778dc38433
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-03-02 21:27:19 +0500

    5-module: TestAsyncThunk

--- next_react_project/src/features/AuthByUsername/model/slice/loginSlice.ts
+++ next_react_project/src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -13,7 +13,6 @@ export const loginSlice = createSlice({
     initialState,
     reducers: {
         setUsername: (state, action: PayloadAction<string>) => {
-            console.log('action', action);
             state.username = action.payload;
         },
         setPassword: (state, action: PayloadAction<string>) => {
