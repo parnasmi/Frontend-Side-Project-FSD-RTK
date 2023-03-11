3d939481ef3555493a6334d9153ad8e54ba371b3
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-03-11 16:46:04 +0500

    6-module: write tests for Profile module

--- next_react_project/src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ next_react_project/src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -18,9 +18,20 @@ const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />
 export const Light = Template.bind({});
 Light.args = {
 };
-
-Light.decorators = [StoreDecorator({ user: { authData: {} } })];
+Light.decorators = [
+    StoreDecorator({
+        user: { authData: {} },
+    }),
+];
 
 export const Dark = Template.bind({});
 Dark.args = {};
 Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({ user: { authData: {} } })];
+
+export const NoAuth = Template.bind({});
+NoAuth.args = {};
+NoAuth.decorators = [
+    StoreDecorator({
+        user: {},
+    }),
+];
