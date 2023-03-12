75aacdb43cfd6518cf8c22ea9883060ad1f41462
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-03-12 20:12:59 +0500

    add .fttemplates

---
+++ .fttemplates/TS Component/[FTName].tsx
@@ -0,0 +1,20 @@
+// template-folder-name -> [FTName].tsx
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/libs';
+import cls from './[FTName].module.scss';
+
+interface [FTName]Props {
+   className?: string;
+}
+
+export const [FTName]: FC<[FTName]Props> = (props) => {
+   const { className } = props;
+   const { t } = useTranslation()
+
+   return (
+      <div className={classNames(cls.[FTName | camelcase], {}, [className])}>
+
+      </div>
+   );
+}
