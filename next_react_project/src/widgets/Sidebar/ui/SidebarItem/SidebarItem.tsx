e3d35a43559334ac7ce6d8c277fa00b41588b8a2
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-03-21 09:51:51 +0500

    8-module: sidebar upgrade

--- next_react_project/src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ next_react_project/src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -1,10 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { memo } from 'react';
-import { SidebarItemType } from 'widgets/Sidebar/model/items';
 import { classNames } from 'shared/libs';
 import { useSelector } from 'react-redux';
 import { getUserAuthData } from 'entities/User';
+import { SidebarItemType } from '../../model/types/sidebar';
 import cls from './SidebarItem.module.scss';
 
 interface SidebarItemProps {
