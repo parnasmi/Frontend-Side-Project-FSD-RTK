ee6228989ea4c57316dbe27d9cc6f5d6a417f3c1
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-05-16 12:10:19 +0500

    11-module:73-implement-access-by-roles

---
+++ next_react_project/src/pages/AdminPanelPage/ui/AdminPanelPage.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
+import { Theme } from 'shared/contexts/theme-context';
+import AdminPanelPage from './AdminPanelPage';
+
+export default {
+    title: 'pages/AdminPanelPage',
+    component: AdminPanelPage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof AdminPanelPage>;
+
+const Template: ComponentStory<typeof AdminPanelPage> = () => (
+    <AdminPanelPage />
+);
+
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
---
+++ next_react_project/src/pages/ForbiddenPage/ui/ForbiddenPage.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
+import { Theme } from 'shared/contexts/theme-context';
+import ForbiddenPage from './ForbiddenPage';
+
+export default {
+    title: 'pages/ForbiddenPage',
+    component: ForbiddenPage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ForbiddenPage>;
+
+const Template: ComponentStory<typeof ForbiddenPage> = () => (
+    <ForbiddenPage />
+);
+
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
