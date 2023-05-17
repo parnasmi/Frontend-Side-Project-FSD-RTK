b1afdae37af68ebcc6f7e35b7718a8567a94d1ca
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-05-17 13:45:35 +0500

    11-module:78: update screenshots

--- next_react_project/src/pages/ForbiddenPage/ui/ForbiddenPage.stories.tsx
+++ next_react_project/src/pages/ForbiddenPage/ui/ForbiddenPage.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Theme } from 'shared/contexts/theme-context';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator';
 import ForbiddenPage from './ForbiddenPage';
 
 export default {
@@ -17,7 +18,8 @@ const Template: ComponentStory<typeof ForbiddenPage> = () => (
 
 export const Normal = Template.bind({});
 Normal.args = {};
+Normal.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
