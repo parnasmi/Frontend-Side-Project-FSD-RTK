1df4aa789d91cf7e30905454d1433a1b30a31ba0
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-05-26 11:20:23 +0500

    13-module: fix loki issues

--- next_react_project/src/pages/MainPage/ui/MainPage.tsx
+++ next_react_project/src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,11 @@
 import { useTranslation } from 'react-i18next';
 
-import { Counter } from '@/entities/Counter';
-
 export default function MainPage() {
     const { t } = useTranslation('main');
 
     return (
         <div>
             {t('Главная страница', { ns: 'main' })}
-            <Counter />
         </div>
     );
 }
