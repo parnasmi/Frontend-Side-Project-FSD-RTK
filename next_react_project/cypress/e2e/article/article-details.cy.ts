acf46a333048d2e7169a1d90b995163656f6fcb5
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-06-01 11:39:39 +0500

    14-module: 115 - creating fixtures

--- next_react_project/cypress/e2e/article/article-details.cy.ts
+++ next_react_project/cypress/e2e/article/article-details.cy.ts
@@ -34,4 +34,12 @@ describe('A user enters article details page', () => {
         cy.setRate(4, 'some comment');
         cy.get('[data-selected=true]').should('have.length', 4);
     });
+
+    it('should be able to add rating using stubs(fixtures)', () => {
+        cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' });
+        cy.getByTestId('ArticleDetails.Info');
+        cy.getByTestId('RatingCard').scrollIntoView();
+        cy.setRate(4, 'some comment');
+        cy.get('[data-selected=true]').should('have.length', 4);
+    });
 });
--- next_react_project/cypress/e2e/article/articles-list.cy.ts
+++ next_react_project/cypress/e2e/article/articles-list.cy.ts
@@ -9,4 +9,16 @@ describe('A user enters to the articles page', () => {
         cy.getByTestId('ArticleList').should('exist');
         cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
     });
+
+    it('load articles on stabs(fixtures)', () => {
+        cy.intercept('GET', '**/articles?*', { fixture: 'articles.json' });
+        cy.getByTestId('ArticleList').should('exist');
+        cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+    });
+
+    it.skip('Example of skipped test', () => {
+        cy.getByTestId('ArticleList').should('exist');
+        cy.getByTestId('ArticleListItem').should('have.length.greaterThan', 3);
+        cy.get('asfasf').should('exist');
+    });
 });
