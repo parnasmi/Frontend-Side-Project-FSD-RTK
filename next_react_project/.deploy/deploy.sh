25aae0f6d60a53ac718bd9c2470eef89464513ec
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-06-20 18:56:24 +0500

    14-module: 123-add deploy bash script2

--- next_react_project/.deploy/deploy.sh
+++ next_react_project/.deploy/deploy.sh
@@ -1,4 +1,4 @@
-cd ~/Frontend-Side-Project-FSD-RTK
+cd ~/Frontend-Side-Project-FSD-RTK/next_react_project
 npx webpack --env mode=production apiUrl=https://side-frontend.online/api
 
 cp -rf build/* /var/www/side-frontend/html
