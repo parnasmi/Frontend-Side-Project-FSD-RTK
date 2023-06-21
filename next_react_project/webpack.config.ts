404d03741365dfa4597e2f4c77c8cfd781031dff
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-06-21 15:58:10 +0500

    14-module: 123-add env files to manage api url2

--- next_react_project/webpack.config.ts
+++ next_react_project/webpack.config.ts
@@ -6,6 +6,10 @@ import { BuildPaths, buildWebpackConfigs, BuildEnv } from './config/build';
 import { BuildMode } from './config/build/types/config';
 
 const envPath = `.env.${process.env.NODE_ENV}`;
+console.log('envPath', envPath);
+console.log('process.env', process.env);
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
 require('dotenv').config({ path: envPath });
 
 export default (env: BuildEnv) => {
@@ -22,6 +26,8 @@ export default (env: BuildEnv) => {
 
     const isDev = mode === 'development';
     const PORT = env?.port || 3003;
+
+    console.log('process.env in confg', process.env);
     const apiUrl = process.env.API_URL || 'http://localhost:8001';
 
     const config: Configuration = buildWebpackConfigs({
